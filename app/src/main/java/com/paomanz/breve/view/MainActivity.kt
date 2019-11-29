e744876238e0b9f97fe1123702847907b7d29607
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-11-30 11:51:18 +1300

    change old line comment into block comment to keep changed line tidily documented

--- app/src/main/java/com/paomanz/breve/view/MainActivity.kt
+++ app/src/main/java/com/paomanz/breve/view/MainActivity.kt
@@ -19,9 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        /*setContentView(R.layout.activity_main)
+        * */
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        /*setContentView(R.layout.activity_main)*/
 
         //Set 'Up Arrow' in Appbar
         mainNavController = Navigation.findNavController(this, R.id.mainNavHostFragment)
