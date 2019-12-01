e26e00c68b41b9b9dc1c3aea5712bdfc28b45ee3
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-01 14:20:01 +1300

    Implement ViewModel and ViewModelFactory

---
+++ app/src/main/java/com/paomanz/breve/modelview/RegistrationViewModel.kt
@@ -0,0 +1,10 @@
+package com.paomanz.breve.modelview
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class RegistrationViewModel : ViewModel(){
+    init {
+        Log.i("RegistrationViewModel","RegistrationViewModel CREATED!!!")
+    }
+}
--- app/src/main/java/com/paomanz/breve/view/RegisterFragment.kt
+++ app/src/main/java/com/paomanz/breve/view/RegisterFragment.kt
@@ -2,17 +2,22 @@ package com.paomanz.breve.view
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.paomanz.breve.R
 import com.paomanz.breve.databinding.FragmentRegisterBinding
+import com.paomanz.breve.modelview.RegistrationViewModel
 
 
 class RegisterFragment : Fragment() {
+
+    private lateinit var viewModel: RegistrationViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,12 @@ class RegisterFragment : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentRegisterBinding>(
             inflater,R.layout.fragment_register,container,false
         )
+
+        /** ViewModel Initialise
+         * */
+        viewModel = ViewModelProviders.of(this)[RegistrationViewModel::class.java]
+        Log.i("RegistrationFragment","ViewModelProviders.of CALLED!!!")
+
 
         binding.buttonSubmitRegister.setOnClickListener {
             Toast.makeText(context,"Submit button clicked!",Toast.LENGTH_SHORT).show()
