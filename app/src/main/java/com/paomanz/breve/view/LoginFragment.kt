88667869419ff54f00e41f2d4f03fdcf2dee5a2c
Paolo-Man001 <p.a.2manlunas@gmail.com>
2019-12-07 15:17:06 +1300

    apply update on android studio. update gradle and kotlin (incremental annotation processors compilation plugin)

--- app/src/main/java/com/paomanz/breve/view/LoginFragment.kt
+++ app/src/main/java/com/paomanz/breve/view/LoginFragment.kt
@@ -16,37 +16,37 @@ import com.paomanz.breve.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
-            inflater, R.layout.fragment_login, container, false
-        )
+   override fun onCreateView(
+      inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View? {
+      val binding = DataBindingUtil.inflate<FragmentLoginBinding>(
+         inflater, R.layout.fragment_login, container, false
+      )
 
-        /** SAFE-ARGS:
-        * Extract the SafeArg value(dummyEmail) from MainFragment's Direction class's argument
-        */
-        val args = LoginFragmentArgs.fromBundle(arguments!!)
-        Toast.makeText(context,"Email from MainFragment: ${args.email}",Toast.LENGTH_SHORT).show()
+      /** SAFE-ARGS:
+       * Extract the SafeArg value(dummyEmail) from MainFragment's Direction class's argument
+       */
+      val args = LoginFragmentArgs.fromBundle(arguments!!)
+      Toast.makeText(context, "Email from MainFragment: ${args.email}", Toast.LENGTH_SHORT).show()
 
-        binding.apply {
-            buttonLoginLogin.setOnClickListener {
-                Toast.makeText(context, "Login Button Clicked!", Toast.LENGTH_SHORT).show()
+      binding.apply {
+         buttonLoginLogin.setOnClickListener {
+            Toast.makeText(context, "Login Button Clicked!", Toast.LENGTH_SHORT).show()
 
-                /** Start AccountActivity
-                 * */
-                val intent = Intent(context,AccountActivity::class.java)
-                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK
-                startActivity(intent)
-                activity!!.finish()     // Destroys the MainActivity task
-            }
+            /** Start AccountActivity
+             * */
+            val intent = Intent(context, AccountActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP and Intent.FLAG_ACTIVITY_NEW_TASK
+            startActivity(intent)
+            activity!!.finish()     // Destroys the MainActivity task
+         }
 
-            textViewSignUpLogin.setOnClickListener {
-                it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-            }
-        }
+         textViewSignUpLogin.setOnClickListener {
+            it.findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+         }
+      }
 
-        return binding.root
-    } // End onCreateView
+      return binding.root
+   } // End onCreateView
 }
--- app/src/main/java/com/paomanz/breve/view/MainFragment.kt
+++ app/src/main/java/com/paomanz/breve/view/MainFragment.kt
@@ -17,51 +17,51 @@ import com.paomanz.breve.databinding.FragmentMainBinding
  */
 class MainFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        /* // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)*/
-        val binding = DataBindingUtil.inflate<FragmentMainBinding>(
-            inflater, R.layout.fragment_main, container, false
-        )
+   override fun onCreateView(
+      inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?
+   ): View? {
+      /* // Inflate the layout for this fragment
+      return inflater.inflate(R.layout.fragment_main, container, false)*/
+      val binding = DataBindingUtil.inflate<FragmentMainBinding>(
+         inflater, R.layout.fragment_main, container, false
+      )
 
-        /*SAFE-ARGS:
-        * dummyEmail is the String data we send to LoginFragment for it to use as default in its EditText(email)
-        */
-        val dummyEmail = "johndoe@email.com"
+      /*SAFE-ARGS:
+      * dummyEmail is the String data we send to LoginFragment for it to use as default in its EditText(email)
+      */
+      val dummyEmail = "johndoe@email.com"
 
-        //  Data-binding can encapsulate all the view-objects(buttons) attached(bound) to this class:
-        binding.apply {
-            buttonLoginMain.setOnClickListener { view: View ->
-                // !!! Make sure to reference the '<action> id' to navigate to the right fragment
-                view.findNavController()
+      //  Data-binding can encapsulate all the view-objects(buttons) attached(bound) to this class:
+      binding.apply {
+         buttonLoginMain.setOnClickListener { view: View ->
+            // !!! Make sure to reference the '<action> id' to navigate to the right fragment
+            view.findNavController()
 //                    .navigate(R.id.action_mainFragment_to_loginFragment)  // Without SafeArgs to pass data
-                    .navigate(MainFragmentDirections.actionMainFragmentToLoginFragment(dummyEmail)) // When using SafeArgs to pass data
-            }
-            buttonRegisterMain.setOnClickListener {
-                it.findNavController().navigate(R.id.action_mainFragment_to_registerFragment)
-            }
-            buttonAboutMain.setOnClickListener {
-                it.findNavController().navigate(R.id.action_mainFragment_to_aboutFragment)
-            }
-        }
+               .navigate(MainFragmentDirections.actionMainFragmentToLoginFragment(dummyEmail)) // When using SafeArgs to pass data
+         }
+         buttonRegisterMain.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_registerFragment)
+         }
+         buttonAboutMain.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_aboutFragment)
+         }
+      }
 
-        //------------------------------------------------//
-        return binding.root // Fragments MUST return this.
-    } // End onCreateView
+      //------------------------------------------------//
+      return binding.root // Fragments MUST return this.
+   } // End onCreateView
 
 
-    // !!! Without Data-binding, we can only reference our Buttons inside this lifecycle method.
-    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+   // !!! Without Data-binding, we can only reference our Buttons inside this lifecycle method.
+   /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+       super.onViewCreated(view, savedInstanceState)
 
-        //About Button
-        button_about_main.setOnClickListener {
-            val action = MainFragmentDirections.actionMainFragmentToAboutFragment()
-            Navigation.findNavController(it).navigate(action)
-        }
-    }*/
+       //About Button
+       button_about_main.setOnClickListener {
+           val action = MainFragmentDirections.actionMainFragmentToAboutFragment()
+           Navigation.findNavController(it).navigate(action)
+       }
+   }*/
 
 }
